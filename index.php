709de070e8551ced202c1d59a3ca6e98c1f4d5a5
Furkan Kalkan <furkan.kalkan3@gmail.com>
2015-11-29 21:49:32 +0200

    Redirection problem on github fixed
    Pages on Github doesn't accept POST request, so we changed it to GET request for fix it.

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ if (!empty($_GET['url'])) {
     $redirect = mysqli_fetch_assoc(mysqli_query($connect, $command1));
     $redirect = urldecode($redirect["url_link"]);
 	
-	echo "<script>function go() {window.frames[0].document.body.innerHTML = '<form target=\"_parent\" method=\"post\" action=\"".$redirect."\"></form>';window.frames[0].document.forms[0].submit()}</script><iframe onload=\"window.setTimeout('go()', 99)\" src=\"about:blank\" style=\"visibility:hidden\"></iframe>";
+	echo "<script>function go() {window.frames[0].document.body.innerHTML = '<form target=\"_parent\" method=\"get\" action=\"".$redirect."\"></form>';window.frames[0].document.forms[0].submit()}</script><iframe onload=\"window.setTimeout('go()', 99)\" src=\"about:blank\" style=\"visibility:hidden\"></iframe>";
     
     //header('HTTP/1.1 301 Moved Permanently');
     //header("Location: " . $redirect);
